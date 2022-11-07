10130918b3b9752f5d1a0608f9ccb73737a7e9bd
marijedb <marijedb@live.nl>
2022-11-07 20:23:54 +0100

    changed api address

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").addEventListener("click", function () {
-    fetch("https://apis.scrimba.com/bored/api/activity")
+    fetch("https://www.boredapi.com/api/activity")
         .then(response => response.json())
         .then(data => {
             document.getElementById("activity").textContent = data.activity;
